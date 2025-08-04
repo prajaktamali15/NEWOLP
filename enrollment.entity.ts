0dfcdad93f20a331d0dfc645c9d36e3903643a40
prajaktamali15 <149664190+prajaktamali15@users.noreply.github.com>
2025-08-05 03:39:50 +0530

    Add files via upload

---
+++ enrollment.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Course } from '../../courses/entities/course.entity';
+
+@Entity()
+export class Enrollment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.enrollments)
+  user: User;
+
+  @ManyToOne(() => Course, (course) => course.enrollments)
+  course: Course;
+}
